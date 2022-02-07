f5aaeea7738b80b53935cb0bb804e862c373cad6
Mikko Sipola <N3120@student.jamk.fi>
2022-02-07 12:13:46 +0200

    ex1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -32,24 +32,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]} />
+      <Total  exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   );
 };
