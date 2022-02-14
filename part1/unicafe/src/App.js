1812f071168dd69f016b6dd2ad94cfcbee970486
Mikko Sipola <N3120@student.jamk.fi>
2022-02-14 17:57:09 +0200

    ex1.9 finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 
-const Button = ({handleClick, text}) => (
+const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}> {text} </button>
 );
 
 const Statistics = (props) => {
-  return (
-    <div>
-    <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.bad + props.neutral + props.good}</p>
-      <p>Average: {props.average}</p>
-      <p>Positive: {props.positive}</p>
-    </div>
-  )
+  console.log(props.allFeedbacks.length !== 0);
+
+  if (props.allFeedbacks.length !== 0) {
+    return (
+      <div>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p>all {props.bad + props.neutral + props.good}</p>
+        <p>Average: {props.average}</p>
+        <p>Positive: {props.positive}</p>
+      </div>
+    );
+  }
+  return <div>No feedback given</div>;
 };
 
 const App = () => {
@@ -29,32 +33,40 @@ const App = () => {
 
   const updateValues = (value) => {
     if (value === 1) {
-      setGood(good + 1)
+      setGood(good + 1);
     }
     if (value === 0) {
-      setNeutral(neutral + 1)
+      setNeutral(neutral + 1);
     }
     if (value === -1) {
-      setBad(bad + 1)
+      setBad(bad + 1);
     }
-    setNewFeedback(allFeedbacks.concat(value))
+    setNewFeedback(allFeedbacks.concat(value));
 
     //Count average & positive feedback
     let sum = 0;
-    allFeedbacks.forEach(value => {
-      sum += value      
-    })
-    setAverage(sum/allFeedbacks.length)
-    setPositive(good/allFeedbacks.length * 100)
-  }
+    allFeedbacks.forEach((value) => {
+      sum += value;
+    });
+    setAverage(sum / allFeedbacks.length);
+    setPositive((good / allFeedbacks.length) * 100);
+  };
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => updateValues(1)} text='Good' />
-      <Button handleClick={() => updateValues(0)} text='Neutral' />
-      <Button handleClick={() => updateValues(-1)} text='Bad' />
-      <Statistics good={good} neutral={neutral} bad={bad} average={average} positive={positive} />
+      <Button handleClick={() => updateValues(1)} text="Good" />
+      <Button handleClick={() => updateValues(0)} text="Neutral" />
+      <Button handleClick={() => updateValues(-1)} text="Bad" />
+      <h1>statistics</h1>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        average={average}
+        positive={positive}
+        allFeedbacks={allFeedbacks}
+      />
 
       {console.log(allFeedbacks)}
       {console.log(average)}
