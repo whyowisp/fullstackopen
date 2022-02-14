056909add9d4e3824dd84fcecd88c2345386a36c
Mikko Sipola <N3120@student.jamk.fi>
2022-02-14 17:11:49 +0200

    ex1.7 statistics still lagging, considering finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,14 +4,6 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}> {text} </button>
 )
 
-const Display = (props) => {
-  return (
-    <div>
-      averages: {props.averages.join(' ')}
-    </div>
-  )
-}
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -19,6 +11,7 @@ const App = () => {
   const [bad, setBad] = useState(0);
   const [allFeedbacks, setNewFeedback] = useState([]);
   const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   const updateValues = (value) => {
     if (value === 1) {
@@ -31,15 +24,14 @@ const App = () => {
       setBad(bad + 1)
     }
     setNewFeedback(allFeedbacks.concat(value))
-    countAverage()
-  }
 
-  const countAverage = () => {
+    //Count average
     let sum = 0;
     allFeedbacks.forEach(value => {
       sum += value      
     })
     setAverage(sum/allFeedbacks.length)
+    setPositive(good/allFeedbacks.length * 100)
   }
 
   return (
@@ -52,6 +44,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {bad + neutral + good}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
+      
 
       {console.log(allFeedbacks)}
       {console.log(average)}
