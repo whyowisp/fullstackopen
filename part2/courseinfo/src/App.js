adcd0d48ec074db97a450df8944860c409a0415b
Mikko Sipola <N3120@student.jamk.fi>
2022-03-15 15:59:22 +0200

    ex2.2 finished

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,62 +5,77 @@ const Header = (props) => {
 };
 
 const Course = (props) => {
-  return (    
+  return (
     <div>
       <Header courseName={props.course.name} />
       <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
     </div>
   );
 };
 
 const Content = (props) => {
-  const { parts } = props 
+  const { parts } = props;
   return (
     <div>
-        {parts.map(part => <p key={part.id}><Part name={part.name} exercises={part.exercises}/></p>)}
+      {parts.map((part) => (
+        <p key={part.id}>
+          <Part name={part.name} exercises={part.exercises} />
+        </p>
+      ))}
     </div>
   );
 };
 
-const Part = ({name, exercises}) => {
+const Part = ({ name, exercises }) => {
   return (
-    <p>{name} {exercises} </p>
+    <p>
+      {name} {exercises}{" "}
+    </p>
   );
 };
 
 const Total = (props) => {
-  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>;
+  const { parts } = props;
+
+  const allExercises = parts.map(part => part.exercises);
+  let total = 0;
+  allExercises.forEach(element => {
+    total += element
+  });
+
+  return <b>Total of {total} exercises</b>;
 };
 
 const App = () => {
   const course = {
     id: 1,
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
+        id: 3,
       },
-      { 
-        name: 'Redux',
+      {
+        name: "Redux",
         exercises: 11,
-        id: 4
-      }
-    ]
-  }
+        id: 4,
+      },
+    ],
+  };
 
-  return <Course course={course} />
+  return <Course course={course} />;
 };
 
 export default App;
