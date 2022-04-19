ec0ae4b99eb3adfcc09deafab7406021fd430b1d
Mikko Sipola <N3120@student.jamk.fi>
2022-04-19 17:45:06 +0300

    ex 2.11 finished

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import React from "react";
+import axios from 'axios'
 
 const Filter = ({ handleChange }) => {
   return (
@@ -43,15 +45,26 @@ const PersonForm = ({
 };
 
 const App = () => {
+  /*
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
     { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
     { name: "Dan Abramov", number: "12-43-234345", id: 3 },
     { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  ]);*/
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response)
+        setPersons(response.data)
+      })
+  }, [])
 
   //function handles both name and number
   const addNewItem = (e) => {
