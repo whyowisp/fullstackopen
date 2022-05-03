b5d7070696c93ac04d52e92228f80c5c7783c95e
Mikko Sipola <N3120@student.jamk.fi>
2022-05-03 17:11:27 +0300

    ex3.3 finished

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -33,6 +33,17 @@ app.get("/info", (request, response) => {
   response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
