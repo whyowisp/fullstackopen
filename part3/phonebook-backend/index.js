a8c6caf5018da2dbce87d48c501431e9882f7f42
Mikko Sipola <N3120@student.jamk.fi>
2022-05-03 16:59:48 +0300

    ex3.2 finished

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,34 +1,38 @@
-const { application } = require('express')
-const  express = require('express')
-const app = express()
+const { application } = require("express");
+const express = require("express");
+const app = express();
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (request, response) => {
-    response.send(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.send(persons);
+});
 
-const PORT = 3001
-app.listen(PORT)
+app.get("/info", (request, response) => {
+  response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`);
+});
+
+const PORT = 3001;
+app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
