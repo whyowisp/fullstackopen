6bdd875d258056910e1cbe9e6ae32675158d5545
Mikko Sipola <N3120@student.jamk.fi>
2022-05-04 17:20:16 +0300

    ex3.8 finished

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,10 +1,17 @@
-const { application } = require("express")
+const { application, response } = require("express")
 const express = require("express")
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+
+morgan.token(
+  "resBody", (req, res) => {
+    return JSON.stringify(req.body)
+  } 
+);
+app.use(morgan(':resBody'))
 
 let persons = [
   {
