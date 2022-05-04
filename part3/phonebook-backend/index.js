d9910fccbda7f8ba2d6b7bfd8869d67d07e79458
Mikko Sipola <N3120@student.jamk.fi>
2022-05-04 14:08:22 +0300

    ex 3.6 finished

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,6 @@
-const { application } = require("express");
-const express = require("express");
-const app = express();
+const { application } = require("express")
+const express = require("express")
+const app = express()
 
 app.use(express.json())
 
@@ -25,55 +25,63 @@ let persons = [
     name: "Mary Poppendieck",
     number: "39-23-6423122",
   },
-];
+]
 
 app.get("/api/persons", (request, response) => {
-  response.send(persons);
-});
+  response.send(persons)
+})
 
 app.get("/info", (request, response) => {
-  response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`);
-});
+  response.send(
+    `Phonebook has info for ${persons.length} people <br> ${new Date()}`
+  )
+})
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+  const id = Number(request.params.id)
+  const person = persons.find((person) => person.id === id)
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id)
 
-    response.status(202).end()
+  response.status(202).end()
 })
 
-app.post('/api/persons', (request, response) => {
-    const newId = Math.floor(Math.random() * 1000)
-    const body = request.body
+app.post("/api/persons", (request, response) => {
+  const newId = Math.floor(Math.random() * 1000)
+  const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    })
+  }
+  //Creates new array with names only and checks if it includes name in question
+  if (persons.map((person) => person.name).includes(body.name)) {
+    return response.status(400).json({
+      error: "name exists already",
+    })
+  }
 
-    const newPerson = {
-        id: newId,
-        name: body.name,
-        number: body.number
-    }
+  const newPerson = {
+    id: newId,
+    name: body.name,
+    number: body.number,
+  }
 
-    persons = persons.concat(newPerson)
+  persons = persons.concat(newPerson)
 
-    response.json(newPerson)
+  response.json(newPerson)
 })
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
