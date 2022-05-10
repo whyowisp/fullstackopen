1d5ac27758ec41061cf5cb5ddf8389f47800941c
Mikko Sipola <N3120@student.jamk.fi>
2022-05-10 17:08:01 +0300

    ex 3.17 finished

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,22 +1,21 @@
-require('dotenv').config()
+require("dotenv").config()
 const { application, response } = require("express")
 const express = require("express")
-const morgan = require('morgan')
-const cors = require('cors')
+const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
-const Person = require('./models/person')
+const Person = require("./models/person")
+const { updateMany } = require("./models/person")
 
-app.use(express.static('build'))
+app.use(express.static("build"))
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan("tiny"))
 app.use(cors())
 
-morgan.token(
-  "resBody", (req, res) => {
-    return JSON.stringify(req.body)
-  } 
-);
-app.use(morgan(':resBody'))
+morgan.token("resBody", (req, res) => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(":resBody"))
 
 app.get("/info", (request, response) => {
   response.send(
@@ -26,66 +25,88 @@ app.get("/info", (request, response) => {
 
 app.get("/api/persons", (request, response) => {
   Person.find({})
-    .then(persons => {
+    .then((persons) => {
       if (persons) {
         response.json(persons)
       } else {
         response.status(204).end()
       }
-  })
-  .catch(error => next(error))
+    })
+    .catch((error) => next(error))
 })
 
 app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id
 
   Person.findById(id)
-  .then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id
 
   Person.findByIdAndRemove(id)
-    .then(result => {
+    .then((result) => {
       response.status(204).end()
     })
-    .catch(error => next(error))
-  })
+    .catch((error) => next(error))
+})
 
 app.post("/api/persons", (request, response) => {
   const body = request.body
 
   if (body.name === undefined || body.number === undefined) {
-    return response.status(400).json({error: 'content missing'})
+    return response.status(400).json({ error: "content missing" })
   }
 
   const person = new Person({
     name: body.name,
-    number: body.number
+    number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-  .catch(error => next(error))
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => {
+      next(error)
+    })
 })
 
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
   }
-  if (error.name === 'DocumentNotFoundError') {
-    return response.status(404).send({ error: 'document you tried to save was not found'})
+  if (error.name === "DocumentNotFoundError") {
+    return response
+      .status(404)
+      .send({ error: "document you tried to save was not found" })
   }
   next(error)
 }
@@ -93,7 +114,7 @@ const errorHandler = (error, request, response, next) => {
 app.use(errorHandler)
 
 const PORT = process.env.PORT
-app.listen(PORT), () => {
-  console.log(`Server running on port ${PORT}`)
-}
-
+app.listen(PORT),
+  () => {
+    console.log(`Server running on port ${PORT}`)
+  }
