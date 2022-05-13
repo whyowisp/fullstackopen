6c8a48d40dab6232e5a247b8c2d9fa945d808e3c
Mikko Sipola <N3120@student.jamk.fi>
2022-05-13 13:50:58 +0300

    ex 4.2 viable version, 90% complete

---
+++ part4/blog-list/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+logger.info('connecting to ', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+//app.use(express.static('build'))
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
