382e186bd55e80582b697196dcebc4326f66f185
Mikko Sipola <N3120@student.jamk.fi>
2022-05-13 17:51:05 +0300

    ex 4.4 completed

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,7 +1,13 @@
+const totalLikes = (blogs) => {
+  const reducer = blogs.reduce((sumOfLikes, blog) => sumOfLikes + blog.likes, 0)
+  return reducer
+}
+
 const dummy = (blogs) => {
   return 1
 }
 
 module.exports = {
+  totalLikes,
   dummy
 }
