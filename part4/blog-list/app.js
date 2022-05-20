153441704044e49c89c08e417b75b78ef5e9b266
Mikko Sipola <N3120@student.jamk.fi>
2022-05-20 09:56:17 +0300

    ex4.19 completed (forgot to push necessary stuff)

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -25,5 +26,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
