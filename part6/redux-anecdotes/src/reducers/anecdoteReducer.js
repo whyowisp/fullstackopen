d65a6bc9c8c1100b4f78b0529d503251621c9786
whyowisp <95976862+whyowisp@users.noreply.github.com>
2022-06-14 14:16:42 +0300

    after 6.14 cleaning

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,74 +1,53 @@
-
-/*const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]*/
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-//const initialState = anecdotesAtStart.map(asObject)
-
 const sortDescending = (state) => {
   state.sort((a, b) => b.votes - a.votes) //better reverse sort method than doing sort().reverse()
 }
 
 const reducer = (state = [], action) => {
-  //console.log('state now: ', state)
-  //console.log('action', action)
-
   switch (action.type) {
-    case 'UPVOTE': {
+    case "UPVOTE": {
       const id = action.data.id
-      const anecdoteToChange = state.find(anecdote => anecdote.id === id)
-      const changedAnecdote = { ...anecdoteToChange, votes: anecdoteToChange.votes + 1 }
-      const updatedState = state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+      const anecdoteToChange = state.find((anecdote) => anecdote.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1,
+      }
+      const updatedState = state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
       sortDescending(updatedState)
       return updatedState
     }
-    case 'CREATE': {
-      return [...state, action.data]
+    case "CREATE": {
+      return [...state, action.data.content]
     }
-    case 'INITIALIZE': {
+    case "INITIALIZE": {
       return action.data.content
     }
-    default: return state
+    default:
+      return state
   }
 }
 
 export const upvote = (id) => {
   return {
-    type: 'UPVOTE',
-    data: { id }
+    type: "UPVOTE",
+    data: { id },
   }
 }
 
 export const createNew = (content) => {
   return {
-    type: 'CREATE',
+    type: "CREATE",
     data: {
       content,
-      id: getId(),
-      votes: 0
-    }
+    },
   }
 }
 
 export const setAnecdotes = (content) => {
   return {
-    type: 'INITIALIZE',
-    data: { content }
+    type: "INITIALIZE",
+    data: { content },
   }
 }
 
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
 import messageReducer from './reducers/messageReducer'
 import filterReducer from './reducers/filterReducer'
-import anecdoteService from './services/anecdotes'
-
 
 const store = configureStore({
   reducer: {
@@ -12,7 +10,5 @@ const store = configureStore({
     filter: filterReducer 
   }
 })
-
-anecdoteService.getAll().then(anecdotes => store.dispatch(setAnecdotes(anecdotes)))
 
 export default store 
